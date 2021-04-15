28a820a61b1692a843801a5002595ad184de63b3
javiddelossantos5 <javiddelossantos5@gmail.com>
2021-04-14 22:55:14 -0600

    added checkpassword to check if the password was incorrect or matches the passwords

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -60,6 +60,29 @@ router.post('/', (req, res) => {
 //   ("Lernantino", "lernantino@gmail.com", "password1234");
 
 
+router.post('/login', (req, res) => {
+    // expect {email: 'lernantino@gmail.com', password: 'password1234'}
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: `No user with that email address!`});
+            return;
+        }
+
+        //Verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: `Incorrect password` });
+            return;
+        }
+        res.json({ user: dbUserData, message: `You are now logged in` });
+        
+    });
+});
+
 //PUT /api/user/1
 router.put('/:id', (req, res) => {
     //expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}

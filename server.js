ceb92817bd6a688492df333e216a1d4c30a11ee9
javiddelossantos5 <javiddelossantos5@gmail.com>
2021-04-21 23:43:39 -0600

    added and login and log out functionalty that stores in a cookie

--- server.js
+++ server.js
@@ -1,16 +1,38 @@
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
-const router = require('./routes/api/user-routes');
+const router = require('./controllers/api/user-routes');
+const { Sequelize } = require('sequelize/types');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //turn on routes
 app.use(routes);
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
 
 //turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
